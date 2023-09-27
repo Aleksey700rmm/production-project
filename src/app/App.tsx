325d5d8f47da61a796b58cc6ffd9f8d35f43a7b1
Aleksey700rmm <ghavrilov199@gmail.com>
2023-09-27 21:49:13 +0300

    Add stylelint, i18n, plugin for eslint styles

---
+++ src/app/App.tsx
@@ -0,0 +1,29 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import './styles/index.scss';
+import { Suspense } from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { useTranslation } from 'react-i18next';
+
+const App = () => {
+    const { theme } = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
